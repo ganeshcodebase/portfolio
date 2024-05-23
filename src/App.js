978ff7386b010e6579d808da05ef58cb4635e4b7
Ganesh <ganesh@moneybloom.in>
2024-05-23 15:14:16 +0530

    Update: Inspect disabled for browser.

--- src/App.js
+++ src/App.js
@@ -8,27 +8,27 @@ import Experience from "./Pages/Experience";
 import Projects from "./Pages/Projects";
 import Contact from "./Pages/Contact";
 const App = () => {
-  // useEffect(() => {
-  //   document.addEventListener("contextmenu", (e) => e.preventDefault());
-  //   function ctrlShiftKey(e, keyCode) {
-  //     return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-  //   }
-  //   const handleKeyDown = (e) => {
-  //     if (
-  //       e.keyCode === 123 ||
-  //       ctrlShiftKey(e, "I") ||
-  //       ctrlShiftKey(e, "J") ||
-  //       ctrlShiftKey(e, "C") ||
-  //       (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
-  //     )
-  //       e.preventDefault();
-  //   };
-  //   document.addEventListener("keydown", handleKeyDown);
-  //   return () => {
-  //     document.removeEventListener("contextmenu", (e) => e.preventDefault());
-  //     document.removeEventListener("keydown", handleKeyDown);
-  //   };
-  // }, []);
+  useEffect(() => {
+    document.addEventListener("contextmenu", (e) => e.preventDefault());
+    function ctrlShiftKey(e, keyCode) {
+      return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+    }
+    const handleKeyDown = (e) => {
+      if (
+        e.keyCode === 123 ||
+        ctrlShiftKey(e, "I") ||
+        ctrlShiftKey(e, "J") ||
+        ctrlShiftKey(e, "C") ||
+        (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+      )
+        e.preventDefault();
+    };
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("contextmenu", (e) => e.preventDefault());
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
     <>
       <section id="/">
